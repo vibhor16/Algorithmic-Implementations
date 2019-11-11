a4f34b08a946eee4a66dd285ee9231b227f56b3b
vibhor shukla <vibhorshukla@vibhors-MacBook-Pro.local>
2019-11-11 03:16:32 -0500

    Climbing Stairs

---
+++ 70ClimbingStairs/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1)
+            return 1;
+        
+        int ar[] = new int[n+1];
+        ar[0] = 0;
+        ar[1] = 1;
+        ar[2] = 2;
+        for(int i=3;i<=n;i++){
+            ar[i] = ar[i-1] + ar[i-2];
+        }
+        
+        return ar[n];
+    }
+}
